d0b242dff94f8b1eb43d7f2e1236e8de5b43a66d
Tran Anh Son <s3926557@rmit.edu.vn>
2024-06-18 22:24:35 +0700

    Added Project

---
+++ PCCleaner/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PCCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cleanTempBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cleanNvidiaBtn = new System.Windows.Forms.Button();
+            this.cleanPrefetchBtn = new System.Windows.Forms.Button();
+            this.cleanWindowsUpdateBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cleanTempBtn
+            // 
+            this.cleanTempBtn.BackColor = System.Drawing.SystemColors.Window;
+            resources.ApplyResources(this.cleanTempBtn, "cleanTempBtn");
+            this.cleanTempBtn.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.cleanTempBtn.Name = "cleanTempBtn";
+            this.cleanTempBtn.UseVisualStyleBackColor = false;
+            this.cleanTempBtn.Click += new System.EventHandler(this.CleanTempFolder);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // cleanNvidiaBtn
+            // 
+            this.cleanNvidiaBtn.BackColor = System.Drawing.SystemColors.Window;
+            resources.ApplyResources(this.cleanNvidiaBtn, "cleanNvidiaBtn");
+            this.cleanNvidiaBtn.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.cleanNvidiaBtn.Name = "cleanNvidiaBtn";
+            this.cleanNvidiaBtn.UseVisualStyleBackColor = false;
+            this.cleanNvidiaBtn.Click += new System.EventHandler(this.CleanNvidiaCache);
+            // 
+            // cleanPrefetchBtn
+            // 
+            this.cleanPrefetchBtn.BackColor = System.Drawing.SystemColors.Window;
+            resources.ApplyResources(this.cleanPrefetchBtn, "cleanPrefetchBtn");
+            this.cleanPrefetchBtn.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.cleanPrefetchBtn.Name = "cleanPrefetchBtn";
+            this.cleanPrefetchBtn.UseVisualStyleBackColor = false;
+            this.cleanPrefetchBtn.Click += new System.EventHandler(this.CleanPrefetch);
+            // 
+            // cleanWindowsUpdateBtn
+            // 
+            this.cleanWindowsUpdateBtn.BackColor = System.Drawing.SystemColors.Window;
+            resources.ApplyResources(this.cleanWindowsUpdateBtn, "cleanWindowsUpdateBtn");
+            this.cleanWindowsUpdateBtn.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.cleanWindowsUpdateBtn.Name = "cleanWindowsUpdateBtn";
+            this.cleanWindowsUpdateBtn.UseVisualStyleBackColor = false;
+            this.cleanWindowsUpdateBtn.Click += new System.EventHandler(this.CleanWindowsUpdatePackages);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cleanWindowsUpdateBtn);
+            this.Controls.Add(this.cleanPrefetchBtn);
+            this.Controls.Add(this.cleanNvidiaBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cleanTempBtn);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button cleanTempBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cleanNvidiaBtn;
+        private System.Windows.Forms.Button cleanPrefetchBtn;
+        private System.Windows.Forms.Button cleanWindowsUpdateBtn;
+    }
+}
+
---
+++ PCCleaner/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PCCleaner
+{
+    public partial class Form1 : Form
+    {
+        int FilesDeleted = 0;
+        int FoldersDeleted = 0;
+        int TotalFiles = 0;
+
+        public Form1() => InitializeComponent();
+
+        private void ResetCounter()
+        {
+            FilesDeleted = 0;
+            FoldersDeleted = 0;
+            TotalFiles = 0;
+        }
+
+        private Boolean IsFolderEmpty(DirectoryInfo folder)
+        {
+            return folder.GetFiles().Length == 0 && folder.GetDirectories().Length == 0;
+        }
+
+        private void CleanTempFolder(object sender, EventArgs e)
+        {
+            ResetCounter();
+            DirectoryInfo tempDirectory = new DirectoryInfo(Path.GetTempPath());
+
+            TotalFiles = tempDirectory.GetFiles().Length + tempDirectory.GetDirectories().Length;
+
+            foreach (FileInfo file in tempDirectory.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                    FilesDeleted++;
+                }
+                catch { }
+            }
+
+            foreach (DirectoryInfo folder in tempDirectory.GetDirectories())
+            {
+                try
+                {
+                    folder.Delete(true);
+                    FoldersDeleted++;
+                }
+                catch { }
+            }
+
+            MessageBox.Show(FilesDeleted + " files and " + FoldersDeleted + " folders deleted of total " + TotalFiles + " files!");
+        }
+
+        private void CleanNvidiaCache(object sender, EventArgs e)
+        {
+            ResetCounter();
+            string nvidiaCachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "..", "LocalLow", "NVIDIA", "PerDriverVersion", "DXCache");
+
+            if (Directory.Exists(nvidiaCachePath))
+            {
+                DirectoryInfo nvidiaCacheDirectory = new DirectoryInfo(nvidiaCachePath);
+
+                if (IsFolderEmpty(nvidiaCacheDirectory))
+                {
+                    MessageBox.Show("NVIDIA DXCache directory is already empty!");
+                    return;
+                }
+
+                TotalFiles = nvidiaCacheDirectory.GetFiles().Length;
+
+                foreach (FileInfo file in nvidiaCacheDirectory.GetFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                        FilesDeleted++;
+                    }
+                    catch { }
+                }
+
+                MessageBox.Show(FilesDeleted + " files deleted of total " + TotalFiles + " files!");
+            }
+            else
+            {
+                MessageBox.Show("NVIDIA DXCache directory does not exist on this machine.");
+            }
+        }
+
+        private void CleanPrefetch(object sender, EventArgs e)
+        {
+            ResetCounter();
+            DirectoryInfo prefetchDirectory = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Prefetch"));
+
+            if (IsFolderEmpty(prefetchDirectory))
+            {
+                MessageBox.Show("Prefetch directory is already empty!");
+                return;
+            }
+
+            TotalFiles = prefetchDirectory.GetFiles().Length;
+
+            foreach (FileInfo file in prefetchDirectory.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                    FilesDeleted++;
+                }
+                catch { }
+            }
+
+            foreach (DirectoryInfo folder in prefetchDirectory.GetDirectories())
+            {
+                try
+                {
+                    folder.Delete(true);
+                    FoldersDeleted++;
+                }
+                catch { }
+            }
+
+            MessageBox.Show("Deleted " + FilesDeleted + " files and " + FoldersDeleted + " folders of total " + TotalFiles + " files!");
+        }
+
+        private void CleanWindowsUpdatePackages(object sender, EventArgs e)
+        {
+            ResetCounter();
+            DirectoryInfo windowsUpdatePackagesDirectory = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "SoftwareDistribution", "Download"));
+
+            if (IsFolderEmpty(windowsUpdatePackagesDirectory))
+            {
+                MessageBox.Show("Windows Update Packages directory is already empty!");
+                return;
+            }
+
+            TotalFiles = windowsUpdatePackagesDirectory.GetFiles().Length + windowsUpdatePackagesDirectory.GetDirectories().Length;
+
+            foreach (FileInfo file in windowsUpdatePackagesDirectory.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                    FilesDeleted++;
+                }
+                catch { }
+            }
+
+            foreach (DirectoryInfo folder in windowsUpdatePackagesDirectory.GetDirectories())
+            {
+                try
+                {
+                    folder.Delete(true);
+                    FoldersDeleted++;
+                }
+                catch { }
+            }
+
+            MessageBox.Show(FilesDeleted + " files and " + FoldersDeleted + " folders deleted of total " + TotalFiles + " files!");
+        }
+    }
+}
