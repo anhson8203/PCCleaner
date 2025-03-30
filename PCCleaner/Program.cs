7d1f8a68b5b75d1d83540498000a6abde3222091
Tran Anh Son <s3926557@rmit.edu.vn>
2025-03-30 19:03:05 +0700

    Only 1 instance can be running at a time now

--- PCCleaner/Program.cs
+++ PCCleaner/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PCCleaner
@@ -12,11 +10,21 @@ namespace PCCleaner
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            using (var mutex = new Mutex(false, "PCCleanerSingletonMutex", out var createdNew))
+            {
+                // Only the illiterate don't understand this mutex.
+                if (!createdNew)
+                {
+                    MessageBox.Show("Another instance of PC Cleaner is already running.", "PC Cleaner", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
